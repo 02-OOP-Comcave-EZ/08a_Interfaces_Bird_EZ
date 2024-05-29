f392a6e6cbcded3d4fb28bd1c573fcd571276c1e
Esther <esther.zirkler@gmail.com>
2024-05-29 12:59:51 +0200

    05 / Bird

---
+++ com/cc/com/cc/java/App.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        output(bird.fly());
+        output(bird.hasFeathers());
+     
+    }
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+
+}
+
